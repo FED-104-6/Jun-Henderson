4cacab4b73d8753f29ebc7029937055051e678bc
daniel furukawa <danifuruka@gmail.com>
2025-08-18 01:11:16 -0700

    register component finished and database added

--- Jun-Henderson-Project/src/app/app.routes.ts
+++ Jun-Henderson-Project/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './features/auth/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: RegisterComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '**', redirectTo: '' },
+];
