51673f796ce35938e075b5b148f7c8feda3f428b
henderson <hendersonhfb@gmail.com>
2025-08-19 05:07:03 -0700

    feat(new-flat): add model, service and component

--- Jun-Henderson-Project/src/app/app.routes.ts
+++ Jun-Henderson-Project/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './features/auth/register/register';
+import { NewFlatComponent } from './features/new-flat/new-flat';
 
 export const routes: Routes = [
   { path: '', component: RegisterComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'new-flat', component: NewFlatComponent },
   { path: '**', redirectTo: '' },
 ];
