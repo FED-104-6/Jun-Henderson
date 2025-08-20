00ef5f1e97db0e85c8e601cd753eea4e6e73fe7b
daniel furukawa <danifuruka@gmail.com>
2025-08-19 21:49:58 -0700

    login feature implemented

--- Jun-Henderson-Project/src/app/app.routes.ts
+++ Jun-Henderson-Project/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './features/auth/register/register';
+import { LoginComponent } from './features/auth/login/login';
 
 export const routes: Routes = [
   { path: '', component: RegisterComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
   { path: '**', redirectTo: '' },
 ];
